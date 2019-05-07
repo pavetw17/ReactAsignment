a4ffee59842b4e8e15245f7ea02bb13734a59d13
pavetw17 <yhw0130@autuni.ac.nz>
2019-05-08 01:35:08 +1200

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pictures from './Component/Pictures'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pictures />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Component/Pictures.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Pictures = () => {
+    return <p> Pictures!</p>;
+}
+export default Pictures
