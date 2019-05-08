7aaf6525dff1dc01ab12fdb30e7e532c6828410b
pavetw17 <yhw0130@autuni.ac.nz>
2019-05-09 01:14:47 +1200

    fecth data

--- src/components/ImageComponent/Image.js
+++ src/components/ImageComponent/Image.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import './Image.css'
+import Aux from '../../hoc/Auxiliary/Auxiliary'
 
 const image = (props) => {
-    return ( 
-        <div className="cell"> 
+    const list = {...props.images.data}
+    console.log(list)
+    let imagesData = Object.values(list).map((item) => {
+        console.log(item)
+        return (
+            <div className="cell" key={item.id}> 
             <figure>
-            <img src="http://placehold.it/350x150" className="responsive-image"  />
-
-            <figcaption>
-            
-                <span style={{float:'left'}}>
-                    AAAA
-                </span>
-                <span style={{float:'right'}}>
-                    BBBB
-                </span>
-            </figcaption>
+                <img src={item.images.fixed_width.url} className="responsive-image" alt="1"  />
+                <figcaption>   
+                    <span style={{float:'left'}}>
+                        AAAA
+                    </span>
+                    <span style={{float:'right'}}>
+                        BBBB
+                    </span>
+                </figcaption>
             </figure>
-        </div>
+            </div>
+        );
+    })
+
+
+    return (
+        
+        <Aux>
+            {imagesData}
+        </Aux>
     );
 }
 
--- src/containers/ImageContainer/ImageListContainer.js
+++ src/containers/ImageContainer/ImageListContainer.js
@@ -1,28 +1,30 @@
 import React, {Component} from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 
 import Aux from '../../hoc/Auxiliary/Auxiliary'
 import Image from '../../components/ImageComponent/Image'
 
 class ImageList extends Component {
     state ={
-
+        images: {}
     }
 
     componentDidMount(){
-
+        axios.get('https://api.giphy.com/v1/gifs/trending?api_key=FuG74WHq1QdFUXq62J2gnqpMjeqx5lVa')
+            .then (response => {
+              
+                this.setState ({ images: response.data})
+            })
+            .catch (error => {
+                this.setState ({ error: true })
+            })
     }
 
     render() {
+        // console.log(this.state.images)
         return (
             <Aux>
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
+                <Image images = {this.state.images}/>
             </Aux>
         );
     }
